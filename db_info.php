c6e46854bbcd7f275c4b98828d39bea31d9bd29e
jishant07 <jishanta@gmail.com>
2019-09-10 20:29:01 +0530

    db changed

--- db_info.php
+++ db_info.php
@@ -1,7 +1,7 @@
 <?php 
-	$servername = "localhost";
-	$db_name = "nss_attendance";
-	$user = "root";
-	$pwd = "";
+	$servername = "remotemysql.com";
+	$db_name = "BDT9ICDTVF";
+	$user = "BDT9ICDTVF";
+	$pwd = "R3JHspSrfS";
 	$con = mysqli_connect($servername, $user, $pwd, $db_name); 
  ?>
